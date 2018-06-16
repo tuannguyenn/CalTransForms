f501469f907af4ed312f41246d2038355e688eb0
Tuan Nguyen <tdntuan@gmail.com>
2018-06-16 00:50:16 -0700

    update

---
+++ forms.js
@@ -0,0 +1,40 @@
+$('document').ready( ()=>{
+    /*
+    $('#section1Box').on('click',()=>{
+        $('#section1Contents').toggle();
+    });
+
+    $('#section2Box').on('click',()=>{
+        $('#section2Contents').toggle();
+    });
+
+    $('#section3Box').on('click',()=>{
+        $('#section3Contents').toggle();
+    });
+
+    $('#section4Box').on('click',()=>{
+        $('#section4Contents').toggle();
+    });
+
+    $('#section5Box').on('click',()=>{
+        $('#section5Contents').toggle();
+    });
+
+    $('#section6Box').on('click',()=>{
+        $('#section6Contents').toggle();
+    });
+
+    $('#section7Box').on('click',()=>{
+        $('#section7Contents').toggle();
+    });
+
+    $('#section8Box').on('click',()=>{
+        $('#section8Contents').toggle();
+    });
+
+    $('#section9Box').on('click',()=>{
+        $('#section9Contents').toggle();
+    });
+    */
+                                    
+})
