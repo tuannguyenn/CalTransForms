789159e3c49f2bc52eafb94ecb1e118fcf656088
Tuan Nguyen <tdntuan@gmail.com>
2018-06-19 10:44:47 -0700

    add open / close buttons each section

--- forms.js
+++ forms.js
@@ -1,4 +1,14 @@
 $('document').ready( ()=>{
 
+    //$('.sectionBox h2 button').html('close')
+
+    $('.sectionBox h2 button').click(function(event){
+        $(event.currentTarget).parent().next('.sectionContents').toggle();
+        if ($(event.currentTarget).html() == 'close'){
+            $(event.currentTarget).html('open');
+        } else if($(event.currentTarget).html() == 'open'){
+            $(event.currentTarget).html('close');
+        };
+    });
                                     
-})
+});
