2f777a6132d948248e6ecda839271ab53b7e0121
Tuan Nguyen <tdntuan@gmail.com>
2018-07-05 04:53:15 -0700

    updated Section 1 fields

--- forms.js
+++ forms.js
@@ -13,6 +13,28 @@ $('document').ready( ()=>{
         };
     });
     
+    // this puts in the date for section 1
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+
+
+    var dateControl = document.querySelector('input[type="date"]');
+    dateControl.value = today;
+
 
     // this is the section 3 project cost calculator
     $('#sbc, #lbc, #sdc, #ldc, #srwc, #lrwc, #soc, #loc ').on('input',()=>{
